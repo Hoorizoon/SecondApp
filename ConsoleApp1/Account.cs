da7e29235293b39a23f8891c39ca0dc5ad1f2c10
DESKTOP-92RNAQI\PC <arturjarzab92@gmail.com>
2024-06-26 18:31:48 +0200

    Init

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive {  get; set; }
+        public Account(string login, string password) { 
+            Login = login;
+            Password = password;
+            IsActive = true;
+            IsAdmin = false;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static List<Account> Accounts = new List<Account>();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int select;
+                Console.WriteLine("1.Zaloguj");
+                Console.WriteLine("2.Zarejestruj");
+                select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        LogIn();
+                        break;
+                    case 2:
+                        CreateUser();
+                        break;
+                }
+            }
+        }
+        static void CreateUser()
+        {
+            string login = "", haslo = "", haslo2 = "";
+            bool passwordcorrect = false;
+            while (passwordcorrect == false)
+            {
+                Console.Write("Podaj login: ");
+                login = Console.ReadLine();
+                Console.Write("Podaj haslo: ");
+                haslo = Console.ReadLine();
+                Console.Write("Powtórz haslo: ");
+                haslo2 = Console.ReadLine();
+                if (haslo != haslo2)
+                {
+
+                    Console.WriteLine("Hasła nie są takie same!");
+                }
+                else
+                {
+                    passwordcorrect = true;
+                }
+            }
+            var account = new Account(login, haslo);
+            Accounts.Add(account);
+        }
+
+
+        static void LogIn()
+        {
+            bool w = true;
+            while (w)
+            {
+                Console.Write("Podaj login: ");
+                var login = Console.ReadLine();
+                Console.Write("Podaj haslo: ");
+                var Haslo = Console.ReadLine();
+                var acc = Accounts.Where(x => x.Login == login && x.Password == Haslo).FirstOrDefault();
+                if (acc != null)
+                {
+                    w = false;
+                    Console.WriteLine("AAAAAAAA");
+
+                }
+                else {
+                    Console.Clear();
+                    Console.WriteLine("Błędne hasło lub login!");
+
+                }
+            }
+
+            //bool IsUser = false;    
+            //foreach (var account in Accounts) {
+            //    if (account.Login == login && account.Password == Haslo)
+            //    {
+            //        Console.WriteLine("Pomyślnie zalogowano!:)");
+            //        IsUser = true;
+            //        break;
+            //    }
+            //}
+        }
+    }
+}
